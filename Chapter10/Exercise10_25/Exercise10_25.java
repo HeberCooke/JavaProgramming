7eecb072555f932e150866de499191e6827ecf8f
404kevnotfound <student@CSiMac1-12.local>
2018-10-10 08:13:58 -0600

    chapter 10 replace

--- Chapter10/Exercise10_25/Exercise10_25.java
+++ Chapter10/Exercise10_25/Exercise10_25.java
@@ -1,22 +1,14 @@
-
-// Heber Cooke 		10/8/2018
-// Exercise 10-25
+// Heber Cooke 	10/9/2018
+// Exercise 10_25
 
 public class Exercise10_25 {
 
 	public static void main(String[] args) {
-		String a = "ab#12#453"; 
-		String remove = "#";
-		String[] temp = Split.split(a, remove);
-		for (int i = 0; i < temp.length; i++) {
-			// if  is to remove the comma at the end of the string 
-			if(i <temp.length -1) {
-			System.out.print(temp[i]+ ","+remove+",");
-			}
-			else {
-				System.out.print(temp[i]+ ","+remove);
-			}
-		}// end for loop
-		
-	}// end main-------------
-}// end Split class -------------------
+		String[] s =Split.split("ab#12#453", "#"); // calling the static Split method with the string array
+		// looping the string array to print out return string array
+		for (int i = 0; i <s.length ; i++) {
+			System.out.print(s[i]+",");
+		}
+	}
+}
+
--- Chapter10/Exercise10_25/Split.java
+++ Chapter10/Exercise10_25/Split.java
@@ -1,11 +1,16 @@
 
-
 public class Split {
-	Split(){
-		
+
+	public static String[] split(String s1, String regex) {		// method 
+	String[] temp2 = s1.split(regex);	 // temp string that returns a string array
+	
+	for(int i = 0; i < temp2.length;i++) { // loop to add the regex value back to the string 
+		temp2[i]+=",";  	// adding the comma to the end of each string in the array
+		temp2[i]+= regex;		// adding the regex value to each string in the array
 	}
-	public static String[] split(String s, String regex) {
-		String []temp = s.split(regex);
-		return temp;				
+		return  temp2; 
 	}
-}
+
+	
+}// end class---------------------------------------------------------
+
